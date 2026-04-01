ca6264b06fcfa91c4a2fa607686888f756a4ca5f
kuyheng <kuyheng503@gmail.com>
2026-04-01 13:49:28 +0700

    Remove duplicate backend and fix profile stats typing

--- portfolio/backend/routes/stats.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const auth = require("../middleware/auth");
-const { getStats } = require("../controllers/statsController");
-
-const router = express.Router();
-
-router.get("/", auth, getStats);
-
-module.exports = router;
